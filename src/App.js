9f470f8a1447b74b159b01493ea9a60dde760b4f
Poniks <nadstogakuba@gmail.com>
2020-06-20 16:46:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    city: "Buk",
+  };
+  handleCheckWeather = (e) => {
+    // e.preventDefault();
+    fetch(
+      `api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=d5d322b13b4d9b28c56f700bc6e384f0`
+    )
+      .then((resp) => console.log(resp.json()))
+      // .then((data) => console.log(data))
+      .catch((err) => {
+        console.log("Error Reading data " + err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <form onSubmit={this.handleCheckWeather}>
+          Enter City: <input type="text" />
+          <input type="submit" />
+        </form> */}
+
+        <input type="text" onChange={this.handleCheckWeather} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// d5d322b13b4d9b28c56f700bc6e384f0
